35d6cfbd8aee01372e113da9cfc1fca9e4cdaf46
Gabriela Fonseca <gabmfonseca@gmail.com>
2020-01-27 18:13:32 +0000

    missing shuffle method

--- starter-code/js/memory.js
+++ starter-code/js/memory.js
@@ -1,18 +1,28 @@
 class MemoryGame {
   constructor(cards) {
     this.cards = cards;
-    // Add the rest of the properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
   }
 
-  shuffleCards() {
-    // ...
-  }
+  shuffleCards() {}
 
   checkIfPair(card1, card2) {
-    // ...
+    this.pairsClicked += 1;
+    if (card1 === card2) {
+      this.pairsGuessed += 1;
+      return true;
+    } else {
+      return false;
+    }
   }
 
   isFinished() {
-    // ...
+    if (this.pairsGuessed === this.cards.length / 2) {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
